9fa0eacf7bace1f5c24df9a653b8e47f120cf462
italo van jaarsveldt <italo.decampo@gmail.com>
2025-07-09 15:28:33 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,49 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# puts "Cleaning database..."
+# Restaurant.destroy_all
+
+# puts "Creating restaurants..."
+# Restaurant.create!(name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "Indian")
+# puts "Created Dishoom"
+# Restaurant.create!(name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "Italian")
+# puts "Created Pizza East"
+# Restaurant.create!(name: "Lupa", address: "Durbanville", category: "Italian")
+# puts "Created Lupa"
+# Restaurant.create!(name: "Bunny Chow", address: "India", category: "Indian")
+# puts "Created Bunny Chow"
+# Restaurant.create!(name: "Greco", address: "Greece", category: "Greek")
+# puts "Created Greco"
+
+# # 3. Display a message
+# puts "Finished! Created #{Restaurant.count} restaurants."
+
+require 'faker'
+
+puts "Cleaning database..."
+Review.destroy_all
+Restaurant.destroy_all
+
+puts "🍽 Creating restaurants and reviews..."
+
+Restaurant::CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+
+10.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: Restaurant::CATEGORIES.sample
+  )
+
+  rand(1..3).times do
+    Review.create!(
+      content: Faker::Restaurant.review,
+      rating: rand(0..5),
+      restaurant: restaurant
+    )
+  end
+end
+
+puts "Done seeding!"
